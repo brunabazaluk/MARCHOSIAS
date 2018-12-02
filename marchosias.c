99d47254189d4a3d3033d1b66ba3c40b78f1c63a
brunabazaluk <bruna@opal.localdomain>
2018-12-02 23:47:11 +0000

    sla
    Signed-off-by: brunabazaluk <bruna@opal.localdomain>

--- marchosias.c
+++ marchosias.c
@@ -6,10 +6,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 /*
 
-8b    d8        db        88""Yb      dP""b8     88  88      dP"Yb      .dP"Y8     88        db            .dP"Y8 
-88b  d88       dPYb       88__dP     dP   `"     88  88     dP   Yb     `Ybo."     88       dPYb           `Ybo." 
-88YbdP88      dP__Yb      88"Yb      Yb          888888     Yb   dP     o.`Y8b     88      dP__Yb          o.`Y8b 
-88 YY 88     dP""""Yb     88  Yb      YboodP     88  88      YbodP      8bodP'     88     dP""""Yb         8bodP' 
+8b    d8        db        88""Yb      dP""b8     88  88      dP"Yb      .dP"Y8     88        db        .dP"Y8 
+88b  d88       dPYb       88__dP     dP   `"     88  88     dP   Yb     `Ybo."     88       dPYb       `Ybo." 
+88YbdP88      dP__Yb      88"Yb      Yb          888888     Yb   dP     o.`Y8b     88      dP__Yb      o.`Y8b 
+88 YY 88     dP""""Yb     88  Yb      YboodP     88  88      YbodP      8bodP'     88     dP""""Yb     8bodP' 
 
 */
 ///////////////////////////////////////////////////////////////////////////////////
